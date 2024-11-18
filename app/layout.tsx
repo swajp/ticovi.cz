03c186e86e4e5b3e9d607f89d3dd9a8fbb9df0ba
Miroslav Stejskal <stejskalmira@icloud.com>
2024-11-18 09:21:57 +0100

    vercel analyticss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import localFont from "next/font/local"
 import "./globals.css"
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
@@ -25,7 +26,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="cs">
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                {children}
+                <Analytics />
+            </body>
         </html>
     )
 }
